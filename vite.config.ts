b637d5a900c1125aa55aba792d1b777938d45610
santos40 <humbert777@gmail.com>
2024-10-28 10:27:01 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,13 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 import path from "path";
-import { componentTagger } from 'lovable-tagger';
 
-// https://vitejs.dev/config/
-export default defineConfig(() => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-    componentTagger(),
-  ],
+export default defineConfig({
+  plugins: [react()],
+  base: "/lxgratis10/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
